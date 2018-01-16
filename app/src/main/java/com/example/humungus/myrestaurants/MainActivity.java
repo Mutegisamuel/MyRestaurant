6e3e012cf2cd1695dffbb45be535d7a4c2c70bc1
mutegi <mutegisamuel0@gmail.com>
2018-01-16 10:54:18 +0300

    edited some files

--- app/src/main/java/com/example/humungus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/humungus/myrestaurants/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.humungus.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
@@ -17,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+           public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
     }
