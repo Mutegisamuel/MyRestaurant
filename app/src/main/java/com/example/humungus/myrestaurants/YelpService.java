4b644e7bc4b75e71a1583acb2fbd40d62ef9c6e4
mutegi <mutegisamuel0@gmail.com>
2018-01-24 12:05:51 +0300

    solving api errors

--- app/src/main/java/com/example/humungus/myrestaurants/YelpService.java
+++ app/src/main/java/com/example/humungus/myrestaurants/YelpService.java
@@ -27,15 +27,13 @@ public class YelpService {
 
     public static void findRestaurants(String location, Callback callback) {
 
-        OkHttpClient client = new OkHttpClient.Builder()
-                .build();
-
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
         urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
         String url = urlBuilder.build().toString();
 
 
         Request request = new Request.Builder()
+                .header("Authorization", "Bearer " + Constants.YELP_CONSUMER_KEY)
                 .url(url)
                 .build();
 
